864c146b21da6e10271f3de3b3b81597af375923
Kimmo Kenttälä <kimmo@kenttala.fi>
2013-04-24 19:21:25 +0300

    Use auction price unless buy-it-now only

--- desuhuuto.py
+++ desuhuuto.py
@@ -10,7 +10,12 @@ from bs4 import BeautifulSoup
 class Listing (object):
     def __init__ (self, soup):
         self.name = soup.find("div", class_="spandesc").find("a").string.strip()
-        price_str = soup.find("div", class_="spanprice").find('strong').string
+        price_spans = soup.find("div", class_="spanprice").find_all('span')
+        price_auction = price_spans[0].find('strong')
+        if price_auction is not None:
+            price_str = price_auction.string
+        else:
+            price_str = price_spans[-1].string
         euros, cents = map(int, re.match(ur"(\d+),(\d{2}) €", price_str).groups())
         self.price = 100 * euros + cents
 
@@ -51,7 +56,7 @@ if conn.getcode() == 200:
     fmt = u"Rannekeilmoituksia Huuto.netissä: \2{:d}\2, mediaani: \2{:s}\2, maksimi: \2{:s}\2"
 
     out = fmt.format(len(listings),
-                     format_price(median(prices)),
+                     format_price(int(median(prices))),
                      format_price(max(prices)))
     print out.encode('utf-8')
 conn.close()
